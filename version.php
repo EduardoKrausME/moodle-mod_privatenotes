aa4499c58ac058e8beaaa7106e9cf6486ccf356a
Eduardo Kraus <kraus@eduardokraus.com>
2026-09-15 12:06:33 -0300

    V 1.1.1

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026091400;
-$plugin->release = "1.1.0";
+$plugin->version = 2026091500;
+$plugin->release = "1.1.1";
 $plugin->component = "mod_privatenotes";
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
